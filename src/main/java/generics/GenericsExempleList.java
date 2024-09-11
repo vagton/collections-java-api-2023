00f42f58870f6694b9c40a10c717bd9eb46d42c7
Vagton Alves Ferreira <vagton.ferreira@gmail.com>
2024-09-11 12:32:30 -0300

    Corrige tratamento de tipos no mapa sem generics

--- src/main/java/generics/GenericsExempleList.java
+++ src/main/java/generics/GenericsExempleList.java
@@ -4,30 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenericsExempleList {
-	public static void main(String[] args) {
-		// Exemplo sem Generics
-		List listaSemGenerics = new ArrayList();
-		listaSemGenerics.add("Elemento 1");
-		listaSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
 
-		// Exemplo com Generics
-		List<String> listaGenerics = new ArrayList<>();
-		listaGenerics.add("Elemento 1");
-		listaGenerics.add("Elemento 2");
+    public static void main(String[] args) {
+        // Exemplo sem Generics
+        List listaSemGenerics = new ArrayList();
+        listaSemGenerics.add("Elemento 1");
+        listaSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
 
-		// Iterando sobre a lista com Generics
-		for (String elemento : listaGenerics) {
-			System.out.println(elemento);
-		}
+        // Exemplo com Generics
+        List<String> listaGenerics = new ArrayList<>();
+        listaGenerics.add("Elemento 1");
+        listaGenerics.add("Elemento 2");
 
-		// Iterando sobre a lista sem Generics (necessário fazer cast)
-		for (Object elemento : listaSemGenerics) {
-			if (elemento instanceof String) {
+        // Iterando sobre a lista com Generics
+        for (String elemento : listaGenerics) {
+            System.out.println(elemento);
+        }
+
+        // Iterando sobre a lista sem Generics (necessário fazer cast)
+        for (Object elemento : listaSemGenerics) {
+            if (elemento instanceof String) {
                 String str = (String) elemento;
                 System.out.println(str);
             } else {
                 System.out.println("Elemento não é uma String: " + elemento);
             }
-		}
-	}
+        }
+    }
 }
--- src/main/java/generics/GenericsExempleSet.java
+++ src/main/java/generics/GenericsExempleSet.java
@@ -4,26 +4,31 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class GenericsExempleSet {
-	public static void main(String[] args) {
-		// Exemplo sem Generics
-		Set conjuntoSemGenerics = new HashSet();
-		conjuntoSemGenerics.add("Elemento 1");
-		conjuntoSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
 
-		// Exemplo com Generics
-		Set<String> conjuntoGenerics = new HashSet<>();
-		conjuntoGenerics.add("Elemento 1");
-		conjuntoGenerics.add("Elemento 2");
+    public static void main(String[] args) {
+        // Exemplo sem Generics
+        Set conjuntoSemGenerics = new HashSet();
+        conjuntoSemGenerics.add("Elemento 1");
+        conjuntoSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
 
-		// Iterando sobre o conjunto com Generics
-		for (String elemento : conjuntoGenerics) {
-			System.out.println(elemento);
-		}
+        // Exemplo com Generics
+        Set<String> conjuntoGenerics = new HashSet<>();
+        conjuntoGenerics.add("Elemento 1");
+        conjuntoGenerics.add("Elemento 2");
 
-		// Iterando sobre o conjunto sem Generics (necessário fazer cast)
-		for (Object elemento : conjuntoSemGenerics) {
-			String str = (String) elemento;
-			System.out.println(str);
-		}
-	}
+        // Iterando sobre o conjunto com Generics
+        for (String elemento : conjuntoGenerics) {
+            System.out.println(elemento);
+        }
+
+        // Iterando sobre o conjunto sem Generics (necessário fazer cast)
+        for (Object elemento : conjuntoSemGenerics) {
+            if (elemento instanceof String) {
+                String str = (String) elemento;
+                System.out.println(str);
+            } else {
+                System.out.println("Elemento não é uma String: " + elemento);
+            }
+        }
+    }
 }
